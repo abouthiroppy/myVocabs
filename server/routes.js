5c3c043a61747951f893c1a78aa33ecae256abe6
abouthiroppy <git@about-hiroppy.com>
2015-02-10 21:58:53 +0900

    add tag api route

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
-
+  app.use('/api/tags', require('./api/tag'));
   app.use('/auth', require('./auth'));
   
   // All undefined asset or api routes should return a 404
