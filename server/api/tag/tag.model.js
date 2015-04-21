5f441d40720cd572dbf57a9aee12cb7f6b70a569
abouthiroppy <git@about-hiroppy.com>
2015-04-22 08:18:09 +0900

    add comment

--- server/api/tag/tag.model.js
+++ server/api/tag/tag.model.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+// TODO: add default tag for saving current watching tag
 var tagSchema = new Schema({
   name: String,
   value: String
